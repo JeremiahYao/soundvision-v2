7464c15947673e925017e6489cd62f06df532ada
JeremiahYao <242067m@student.hci.edu.sg>
2026-02-19 21:34:32 +0800

    Update camera.py

--- app/camera.py
+++ app/camera.py
@@ -3,7 +3,7 @@ import cv2
 class Camera:
     def __init__(self):
         print("Camera initialized")
-        self.image_path = "test.jpg"
+        self.image_path = "/content/test.jpg"
 
     def get_frame(self):
         frame = cv2.imread(self.image_path)
